78632ad13e298cc06b5c0141d34aa037e4b2cf2b
Igor <i.regulus@gmail.com>
2016-09-20 17:54:10 +0300

    Add protocols

--- audible-login/ViewController.swift
+++ audible-login/ViewController.swift
@@ -8,20 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
-    let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .red
+        cv.dataSource = self
+        cv.delegate = self
         return cv
     }()
+    
+    let cellID = "cellID"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(collectionView)
+        
         collectionView.anchorToTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        
+        return cell
     }
 
 
